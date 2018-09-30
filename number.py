5edf7ff7f8c2e415b78c747747bd96c615f86dd7
Robert Cai <robert913152750@gmail.com>
2018-09-30 16:24:07 +0800

    add count

--- number.py
+++ number.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
@@ -10,5 +12,6 @@ while True:
 		print('比', num,'大')
 	elif num > r:
 		print('比', num,'小')
+	print('這是你猜的第', count, '次')
 		
 	
