b39112cf8aed292250b022cd7fdbfbee0a66f9e7
Robert Cai <robert913152750@gmail.com>
2018-09-30 16:30:54 +0800

    final version

--- number.py
+++ number.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+star = input('請決定最小值: ')
+end = input('請決定最大值: ')
+star = int(star)
+end = int(end)
+r = random.randint(star,end)
 count = 0
 while True:
 	count += 1
@@ -7,6 +11,7 @@ while True:
 	num = int(num)
 	if num == r:
 		print('恭喜答對!!')
+		print('你用了', count, '次猜對')
 		break
 	elif num < r:
 		print('比', num,'大')
